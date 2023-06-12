7bf2ead045637a77e883048abb4e853bd3bb8eb7
benjaminmeredith <benjamin.meredith97@gmail.com>
2023-06-11 21:30:42 -0700

    Added documentation to the project.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+/**
+ * This is the entry point to my sample app.
+ *
+ * @author Benjamin Meredith
+ * @version 1.1
+ */
 public class Main
 {
+    /**
+     *
+     * @param args command-line args (not used)
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello world,from SDEV 220!");
